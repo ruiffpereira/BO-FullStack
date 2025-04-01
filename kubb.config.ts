6a309620e0a6c04d987a108b58c108e132676b48
Rui Pereira <r_f@live.com.pt>
2025-04-02 00:00:31 +0100

    update

---
+++ kubb.config.ts
@@ -0,0 +1,56 @@
+import { defineConfig } from '@kubb/core'
+import { pluginOas } from '@kubb/plugin-oas'
+import { pluginTs } from '@kubb/plugin-ts'
+import { pluginReactQuery } from '@kubb/plugin-react-query'
+import 'dotenv/config'
+
+// yarn add @kubb/core @kubb/plugin-oas @kubb/plugin-ts @kubb/plugin-react-query @kubb/plugin-client @tanstack/react-query
+
+const configs = [
+  defineConfig({
+    name: 'Customers',
+    root: '.',
+    input: {
+      path: `${process.env.NEXT_PUBLIC_API_BASE_URL}-docs/websites/customers.json`, // URL do Swagger ou OpenAPI
+    },
+    output: {
+      path: './src/server/customers',
+      extension: {
+        '.ts': '.js',
+      },
+    },
+    plugins: [
+      pluginOas(), // Processa o OpenAPI/Swagger
+      pluginTs(), // Gera tipos TypeScript
+      pluginReactQuery({
+        client: {
+          baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+        },
+      }), // Gera hooks para React Query
+    ],
+  }),
+  defineConfig({
+    name: 'Ecommerce',
+    root: '.',
+    input: {
+      path: `${process.env.NEXT_PUBLIC_API_BASE_URL}-docs/websites/ecommerce.json`, // URL do Swagger ou OpenAPI
+    },
+    output: {
+      path: './src/server/ecommerce',
+      extension: {
+        '.ts': '.js',
+      },
+    },
+    plugins: [
+      pluginOas(), // Processa o OpenAPI/Swagger
+      pluginTs(), // Gera tipos TypeScript
+      pluginReactQuery({
+        client: {
+          baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+        },
+      }), // Gera hooks para React Query
+    ],
+  }),
+]
+
+export default configs
